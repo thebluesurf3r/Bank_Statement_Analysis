c3e1a769024edcdb70d6160d4d98a76d8f9fba5b
thebluesurf3r <38047853+thebluesurf3r@users.noreply.github.com>
2024-08-04 16:22:13 +0545

    Update app_v1.py

--- app_v1.py
+++ app_v1.py
@@ -52,6 +52,21 @@ if st.session_state.show_documentation:
     st.markdown(f'''
         <div id="documentation" style="padding: 30px; background-color: black; border-radius: 10px;">
             {doc_content}
+        <style>
+            #documentation {{
+                animation: slideDown 0.5s ease-in-out;
+            }}
+            @keyframes slideDown {{
+                from {{
+                    max-height: 0;
+                    opacity: 0;
+                }}
+                to {{
+                    max-height: 500px;
+                    opacity: 1;
+                }}
+            }}
+        </style>
     ''', unsafe_allow_html=True)
 
 # Caching data loading function
@@ -172,10 +187,6 @@ def extract_name(description):
         re.IGNORECASE
     )
 
-    def normalize_name(name):
-        """ Normalize name by reducing multiple spaces to a single space and stripping. """
-        return re.sub(r'\s+', ' ', name).strip()
-
     # Try to find names with the Amazon pattern first
     amazon_match = amazon_pattern.search(description)
     if amazon_match:
@@ -215,22 +226,58 @@ def extract_name(description):
                                 embedded_matches = embedded_pattern.findall(description)
                                 extracted_name = embedded_matches[0].title() if embedded_matches else 'Other'
 
-    # Normalize extracted name
-    extracted_name = normalize_name(extracted_name)
-    
     # Categorize the extracted name
     categorized_name = categorize_name(extracted_name)
     return categorized_name if categorized_name != 'Other' else extracted_name
 
-# Load the CSV file into a DataFrame
-def load_patterns_from_csv(file_path):
-    df = pd.read_csv(file_path)
-    return pd.Series(df.pattern.values, index=df.category).to_dict()
-
 # Define the categorize_names function
-def categorize_name(extracted_name, patterns_csv='patterns.csv'):
-    # Load patterns from CSV
-    patterns = load_patterns_from_csv(patterns_csv)
+def categorize_name(extracted_name):
+    patterns = {
+        'Vyom': r'\b(vyomdeepans|vyom|vyom deepansh|8447156697|9958121100|fd booked|rd booked|vyomdeepansh-1)\b',
+        'Kanishq Sharma': r'\b(muzicmapass|kanishq(?: sharma)?|kan|9873683245|8433204684)\b',
+        'Kasturi Sharma': r'(?i)\b(kast(?:oori(?:sharma)?|oorisha)?|8789816580|KASTURI SHAR)\b',
+        'Ajay Sharma': r'\b(ajay sharma|9833640145)\b',
+        'Deepak Vishwakarma': r'\b(deepak (?:vishwakarma|kumar vi))\b',
+        'Anandita Jangra': r'\b(anandita(?: jangra)?|8979655500|ananditajangra1)\b',
+        'Dhruv Parashar': r'\b(dhruv parashar)\b',
+        'Karanveer': r'\b(karancr8999|karanveer|971585216969|9646862136)\b',
+        'Karan Talwar': r'\b(karan talwar)\b',
+        'Pragun Magan': r'\b(pragun magan|8447783423)\b',
+        'Yawar Rashid': r'\b(yawar rashid|9956394027)\b',
+        'Hitesh Bhagat': r'\b(hitesh(?: bhagat|bhaga)?|hit|ICICX5879|8447299009)\b',
+        'Akhriebu Pucho': r'\b(akhriebu(?: pucho)?|9582384807)\b',
+        'Bhupesh Jingar': r'\b(bhupesh jingar|darsh jing|ICICX7180)\b',
+        'Vivek Tanti': r'\b(vivek(?: tanti|tanti5)?|9172603649)\b',
+        'Vishal Tanti': r'\b(vishal(?: tanti|tanti|\.tant)?|UTIBX8285|9774973923)\b',
+        'Gaurav Yadav': r'\b(1993ygaurav|Gaurav Yadav)\b',
+        'Jegendra': r'\b(jegendermn7)\b',
+        'Parth Singh': r'\b(parth singh|9910270502)\b',
+        
+        'Dominos': r'\b(dominos)\b',
+        'Bagril Biotech': r'(bagril)',
+        'Cognizant': r'\bCOGNIZANT\b|\bCOGNIZ.*?\b',
+        'Zomato': r'\bZomato\b(?: Media Pr| Ltd)?',
+        'Amazon': r'\amazon|amazon@apl|you are pay|amazon india|amazon pay|amazon seller\b',
+        'Rento Mojo': r'\b(edunetwork|rento|rentomojo|rentomojorazorp|rentomojorentpa)\b',
+        'DBHVN': r'\b(dakshin|dbhvn)\b',
+        'Bookmyshow': r'\b(bookmyshow)\b',
+        'Makemytrip': r'\b(makemytrip)\b',
+        'Flipkart': r'\b(flipka|flipkart)\b',
+        'Swiggy': r'\b(swiggy)\b',
+        'Blinkit': r'\b(grofers|blinkit)\b',
+        'Licious': r'\b(licious)\b',
+
+        'Airtel': r'\b(airtel|bharti|BhartiAirte)\b',
+        'Aditya Birla': r'\b(aditya birla fa|ABFL)\b',
+        'Uber': r'\b(uberrides|uber)\b',
+        'Ola': r'\bola\s+(money|financial)\b',
+        'Netflix': r'\bnetflix(?:\scom)?\b',
+        'YouTube': r'\b(?:sold by\s+youtube|youtube(?:\s?prem)?)\b',
+        'Google': r'\b(google india di)\b',
+        'Paytm Wallet': r'\b(payt|add-money)\b',
+        'Personal Loan': r'\b(SPLN|PDL)\b',
+        'Kotak': r'\bchr?gs?|annual fee|cw fee\b',
+    }
     
     for category, pattern in patterns.items():
         if re.search(pattern, extracted_name, re.IGNORECASE):
