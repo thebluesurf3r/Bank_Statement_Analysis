c6d82be6f2a4a337e385dbc9e393ae30775d1a40
thebluesurf3r <38047853+thebluesurf3r@users.noreply.github.com>
2024-08-06 12:06:12 +0545

    Update app_v1.py

--- app_v1.py
+++ app_v1.py
@@ -17,6 +17,22 @@ st.set_page_config(
     initial_sidebar_state="expanded"
 )
 
+# Google Tag Manager <head> script
+st.markdown("""
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-MS7DW4T');</script>
+    <!-- End Google Tag Manager -->
+</head>
+<body>
+""", unsafe_allow_html=True)
+
 # Google Tag script
 st.markdown("""
     <!-- Google tag (gtag.js) -->
@@ -25,28 +41,18 @@ st.markdown("""
       window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
-
       gtag('config', 'G-YEJPSC7F2W');
     </script>
 """, unsafe_allow_html=True)
 
-# Google Tag Manager <head> script
-st.markdown("""
-    <!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-MS7DW4T');</script>
-<!-- End Google Tag Manager -->
-""", unsafe_allow_html=True)
-
 # Google Tag Manager <body> script
 st.markdown("""
-<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MS7DW4T"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MS7DW4T"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+</body>
+</html>
 """, unsafe_allow_html=True)
 
 
