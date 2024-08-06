5888492fd1e11b43be62c31e2a0fea016c5ec21d
thebluesurf3r <38047853+thebluesurf3r@users.noreply.github.com>
2024-08-06 12:15:55 +0545

    Update app_v1.py

--- app_v1.py
+++ app_v1.py
@@ -21,18 +21,6 @@ st.set_page_config(
 # Google Tag <head> script
 
 google_tag = """
-    <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-MS7DW4T');</script>
-    <!-- End Google Tag Manager -->
-"""
-st.markdown(google_tag, unsafe_allow_html=True)
-
-# Google Tag script
-st.markdown("""
     <!-- Google tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-YEJPSC7F2W"></script>
     <script>
@@ -41,15 +29,32 @@ st.markdown("""
       gtag('js', new Date());
       gtag('config', 'G-YEJPSC7F2W');
     </script>
-""", unsafe_allow_html=True)
+"""
 
-# Google Tag Manager <body> script
-st.markdown("""
+gtm_head = """
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-MS7DW4T');</script>
+    <!-- End Google Tag Manager -->
+"""
+gtm_body = """
     <!-- Google Tag Manager (noscript) -->
     <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MS7DW4T"
     height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
-""", unsafe_allow_html=True)
+"""
+
+# Google Tag
+st.markdown(google_tag, unsafe_allow_html=True)
+
+# GTM <head>
+st.markdown(gtm_head, unsafe_allow_html=True)
+
+# GTM <body>
+st.markdown(gtm_body, unsafe_allow_html=True)
 
 
 # Title and Introduction with custom CSS
